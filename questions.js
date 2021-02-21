0948f0c2613cc0b2ba9fb17a1c5102ad32ab7262
Michael Zhou <myz227@nyu.edu>
2021-02-21 09:09:58 -0800

    Add models and questions with lambda functionalities

---
+++ questions.js
@@ -0,0 +1,492 @@
+module.exports = {
+  /**
+   * When editing your questions pay attention to your punctuation.
+   * Make sure you use question marks.
+   * Make sure the first answer is the correct one.
+   * Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+   */
+  QUESTIONS_EN_GB: [
+    {
+      'Who was the first English ruling queen to be crowned?': [
+        'Mary I',
+        'Queen Matilda',
+        'Elizabeth I',
+        'Lady Jane Grey',
+      ],
+    },
+    {
+      'Which early queen married two kings of Wessex (who were father and son)?': [
+        'Judith of Francia',
+        'Edith Godwin',
+        'Eadgifu',
+        'Eadburh',
+      ],
+    },
+    {
+      'Who was the mother of the Princes in the Tower?': [
+        'Elizabeth Woodville',
+        'Isabella of Gloucester',
+        'Anne Boleyn',
+        'Anne Hyde',
+      ],
+    },
+    {
+      'Which king lost to William the Conqueror in the Battle of Hastings?': [
+        'Harold Godwinson',
+        'Harald Hardrada',
+        'Vortigern',
+        'Aethelraed the Unready',
+      ],
+    },
+    {
+      'Who was the only English ruling queen never to marry?': [
+        'Elizabeth I',
+        'Mary I',
+        'Anne',
+        'Mary II',
+      ],
+    },
+    {
+      'Which Queen only ruled for 9 days?': [
+        'Lady Jane Grey',
+        'Victoria I',
+        'Mary II',
+        'Elizabeth I',
+      ],
+    },
+    {
+      'Which English King only spent 6 months in England out of his 10 year reign?': [
+        'Richard the Lionheart',
+        'Henry VIII',
+        'Henry V',
+        'George II',
+      ],
+    },
+    {
+      'Which two queen consorts of England were also sisters?': [
+        'Isabella of Valois and Catherine of Valois',
+        'Berengaria of Navarre and Joan of Navarre',
+        'Isabella of Angouleme and Eleanor of Provence',
+        'Margaret of France and Isabella of France',
+      ],
+    },
+    {
+      'What is the name of the king who accidentally ordered the death of Thomas Becket?': [
+        'Henry II',
+        'Henry III',
+        'Richard II',
+        'William I',
+      ],
+    },
+    {
+      'Which of these queens was never married to Henry the Eighth?': [
+        'Eleanor of Castile',
+        'Jane Seymour',
+        'Catherine of Aragon',
+        'Anne of Cleves',
+      ],
+    },
+    {
+      'Which of the following kings was killed while out hunting?': [
+        'William II',
+        'Stephen',
+        'Henry II',
+        'Richard II',
+      ],
+    },
+    {
+      'Which English king is Queen Aelfthryth (Elfrida) rumoured to have murdered?': [
+        'Edward the Martyr',
+        'Edward II',
+        'Richard II',
+        'Edward V',
+      ],
+    },
+    {
+      'Which queen of the House of Stuart was not born a Catholic, but converted as an adult?': [
+        'Anne of Denmark (wife of James I)',
+        'Catherine of Braganza (wife of Charles II)',
+        'Mary of Modena (wife of James II)',
+        'Henrietta Maria of France (wife of CHarles I)',
+      ],
+    },
+    {
+      'How many queens of England/the United Kingdom (both consorts and monarchs) have been called Elizabeth?': [
+        'Five',
+        'Three',
+        'Six',
+        'Eight',
+      ],
+    },
+    {
+      'Who lost to Henry Tudor in the War of the Roses?': [
+        'Richard III',
+        'Richard II',
+        'Henry VI',
+        'Edward IV',
+      ],
+    },
+    {
+      'Which king was the main target of the Gunpowder Plot?': [
+        'James I',
+        'Charles I',
+        'James II',
+        'Charles II',
+      ],
+    },
+    {
+      'What was the relationship between Queen Victoria and her predecessor, William the Fourth?': [
+        'She was his niece',
+        'She was his granddaughter',
+        'She was his daughter',
+        'She was his cousin',
+      ],
+    },
+    {
+      'What English queen did Philip of Spain marry?': [
+        'Mary I',
+        'Lade Jane Grey',
+        'Elizabeth I',
+        'Victoria',
+      ],
+    },
+    {
+      'How was Elizabeth I related to Mary I?': [
+        'She was her half-sister',
+        'She was her sister',
+        'She was her cousin',
+        'She was her daughter',
+      ],
+    },
+    {
+      'Which king commissioned the famous Domesday Book survey?': [
+        'William the Conqueror',
+        'Edward the Confessor',
+        'Alfred the Great',
+        'William II',
+      ],
+    },
+    {
+      'Who was known as Beauclerc and was probably the first Norman king to speak fluent English?': [
+        'Henry I',
+        'Stephen',
+        'Henry II',
+        'William II',
+      ],
+    },
+    {
+      'Who had the Stone of Scone removed to London and placed in the Coronation Chair in Westminster Abbey?': [
+        'Edward I (Longshanks)',
+        'Richard I (Lionheart)',
+        'Henry IV (Bolingbroke)',
+        'John (Lackland)',
+      ],
+    },
+    {
+      'Which king founded the personal bodyguard known as the Yeomen of the Guard, which still exists today?': [
+        'Henry VII',
+        'Henry VIII',
+        'Henry V',
+        'Henry VI',
+      ],
+    },
+    {
+      'Who was the first English sovereign to go from England to France without crossing the English Channel?': [
+        'Elizabeth II',
+        'Mary, Queen of Scots',
+        'Bonnie Prince Charlie',
+        'George VI',
+      ],
+    },
+    {
+      'Who was king when the Romanovs were shot and didn\'t admit them to the U.K., even though they were cousins?': [
+        'George V',
+        'Edward VI',
+        'Edward VIII',
+        'Edward VII',
+      ],
+    },
+    {
+      'Which king spoke German but never bothered to learn proper English??': [
+        'George I',
+        'George II',
+        'George IV',
+        'George III',
+      ],
+    },
+    {
+      'Which medieval king was imprisoned and murdered on the orders of his own wife??': [
+        'Edward II',
+        'Richard II',
+        'Edward III',
+        'Henry II',
+      ],
+    },
+    {
+      'Which king lost his crown jewels?': [
+        'King John',
+        'King Richard II',
+        'King Henry I',
+        'King Henry III',
+      ],
+    },
+    {
+      'What was the name of Jane Grey\'s husband who was executed with her in 1553?': [
+        'Guilford Dudley',
+        'Robert Dudley',
+        'Henry Dudley',
+        'Alistair Dudley',
+      ],
+    },
+    {
+      'Which king was known as The Hammer of the Scots?': [
+        'Edward I',
+        'Edward III',
+        'Edward II',
+        'Henry II',
+      ],
+    },
+  ],
+  QUESTIONS_EN_US: [
+    {
+      'Who was the first English ruling queen to be crowned?': [
+        'Mary I',
+        'Queen Matilda',
+        'Elizabeth I',
+        'Lady Jane Grey',
+      ],
+    },
+    {
+      'Which early queen married two kings of Wessex (who were father and son)?': [
+        'Judith of Francia',
+        'Edith Godwin',
+        'Eadgifu',
+        'Eadburh',
+      ],
+    },
+    {
+      'Who was the mother of the Princes in the Tower?': [
+        'Elizabeth Woodville',
+        'Isabella of Gloucester',
+        'Anne Boleyn',
+        'Anne Hyde',
+      ],
+    },
+    {
+      'Which king lost to William the Conqueror in the Battle of Hastings?': [
+        'Harold Godwinson',
+        'Harald Hardrada',
+        'Vortigern',
+        'Aethelraed the Unready',
+      ],
+    },
+    {
+      'Who was the only English ruling queen never to marry?': [
+        'Elizabeth I',
+        'Mary I',
+        'Anne',
+        'Mary II',
+      ],
+    },
+    {
+      'Which Queen only ruled for 9 days?': [
+        'Lady Jane Grey',
+        'Victoria I',
+        'Mary II',
+        'Elizabeth I',
+      ],
+    },
+    {
+      'Which English King only spent 6 months in England out of his 10 year reign?': [
+        'Richard the Lionheart',
+        'Henry VIII',
+        'Henry V',
+        'George II',
+      ],
+    },
+    {
+      'Which two queen consorts of England were also sisters?': [
+        'Isabella of Valois and Catherine of Valois',
+        'Berengaria of Navarre and Joan of Navarre',
+        'Isabella of Angouleme and Eleanor of Provence',
+        'Margaret of France and Isabella of France',
+      ],
+    },
+    {
+      'What is the name of the king who accidentally ordered the death of Thomas Becket?': [
+        'Henry II',
+        'Henry III',
+        'Richard II',
+        'William I',
+      ],
+    },
+    {
+      'Which of these queens was never married to Henry the Eighth?': [
+        'Eleanor of Castile',
+        'Jane Seymour',
+        'Catherine of Aragon',
+        'Anne of Cleves',
+      ],
+    },
+    {
+      'Which of the following kings was killed while out hunting?': [
+        'William II',
+        'Stephen',
+        'Henry II',
+        'Richard II',
+      ],
+    },
+    {
+      'Which English king is Queen Aelfthryth (Elfrida) rumoured to have murdered?': [
+        'Edward the Martyr',
+        'Edward II',
+        'Richard II',
+        'Edward V',
+      ],
+    },
+    {
+      'Which queen of the House of Stuart was not born a Catholic, but converted as an adult?': [
+        'Anne of Denmark (wife of James I)',
+        'Catherine of Braganza (wife of Charles II)',
+        'Mary of Modena (wife of James II)',
+        'Henrietta Maria of France (wife of CHarles I)',
+      ],
+    },
+    {
+      'How many queens of England/the United Kingdom (both consorts and monarchs) have been called Elizabeth?': [
+        'Five',
+        'Three',
+        'Six',
+        'Eight',
+      ],
+    },
+    {
+      'Who lost to Henry Tudor in the War of the Roses?': [
+        'Richard III',
+        'Richard II',
+        'Henry VI',
+        'Edward IV',
+      ],
+    },
+    {
+      'Which king was the main target of the Gunpowder Plot?': [
+        'James I',
+        'Charles I',
+        'James II',
+        'Charles II',
+      ],
+    },
+    {
+      'What was the relationship between Queen Victoria and her predecessor, William the Fourth?': [
+        'She was his niece',
+        'She was his granddaughter',
+        'She was his daughter',
+        'She was his cousin',
+      ],
+    },
+    {
+      'What English queen did Philip of Spain marry?': [
+        'Mary I',
+        'Lade Jane Grey',
+        'Elizabeth I',
+        'Victoria',
+      ],
+    },
+    {
+      'How was Elizabeth I related to Mary I?': [
+        'She was her half-sister',
+        'She was her sister',
+        'She was her cousin',
+        'She was her daughter',
+      ],
+    },
+    {
+      'Which king commissioned the famous Domesday Book survey?': [
+        'William the Conqueror',
+        'Edward the Confessor',
+        'Alfred the Great',
+        'William II',
+      ],
+    },
+    {
+      'Who was known as Beauclerc and was probably the first Norman king to speak fluent English?': [
+        'Henry I',
+        'Stephen',
+        'Henry II',
+        'William II',
+      ],
+    },
+    {
+      'Who had the Stone of Scone removed to London and placed in the Coronation Chair in Westminster Abbey?': [
+        'Edward I (Longshanks)',
+        'Richard I (Lionheart)',
+        'Henry IV (Bolingbroke)',
+        'John (Lackland)',
+      ],
+    },
+    {
+      'Which king founded the personal bodyguard known as the Yeomen of the Guard, which still exists today?': [
+        'Henry VII',
+        'Henry VIII',
+        'Henry V',
+        'Henry VI',
+      ],
+    },
+    {
+      'Who was the first English sovereign to go from England to France without crossing the English Channel?': [
+        'Elizabeth II',
+        'Mary, Queen of Scots',
+        'Bonnie Prince Charlie',
+        'George VI',
+      ],
+    },
+    {
+      'Who was king when the Romanovs were shot and didn\'t admit them to the U.K., even though they were cousins?': [
+        'George V',
+        'Edward VI',
+        'Edward VIII',
+        'Edward VII',
+      ],
+    },
+    {
+      'Which king spoke German but never bothered to learn proper English??': [
+        'George I',
+        'George II',
+        'George IV',
+        'George III',
+      ],
+    },
+    {
+      'Which medieval king was imprisoned and murdered on the orders of his own wife??': [
+        'Edward II',
+        'Richard II',
+        'Edward III',
+        'Henry II',
+      ],
+    },
+    {
+      'Which king lost his crown jewels?': [
+        'King John',
+        'King Richard II',
+        'King Henry I',
+        'King Henry III',
+      ],
+    },
+    {
+      'What was the name of Jane Grey\'s husband who was executed with her in 1553?': [
+        'Guilford Dudley',
+        'Robert Dudley',
+        'Henry Dudley',
+        'Alistair Dudley',
+      ],
+    },
+    {
+      'Which king was known as The Hammer of the Scots?': [
+        'Edward I',
+        'Edward III',
+        'Edward II',
+        'Henry II',
+      ],
+    },
+  ],
+};
